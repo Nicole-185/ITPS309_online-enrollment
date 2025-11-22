ff9c01fa11e977fa023807d9279f7c5e14558c19
Nicole-185 <gianan.jeannenicole.dll@gmail.com>
2025-11-22 18:58:16 +0800

    Delete enrollment directory

--- enrollment/online_enrollment/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import  StudentInfo, EducationalBackground, Account, FamilyBackground, Subjects
-
-admin.site.register(StudentInfo)
-admin.site.register(EducationalBackground)
-admin.site.register(Account)
-admin.site.register(FamilyBackground)
-admin.site.register(Subjects)
